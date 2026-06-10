0ff5394822a661459c0ca75cf21047e3bd26570f
David Strozzi <strozzi2@llnl.gov>
2026-06-09 17:19:29 -0700

    example_read.py: removed unused import, made "".format() into f""

--- examples/example_read.py
+++ examples/example_read.py
@@ -2,14 +2,13 @@
 example_read.py opens and lists the contents of files produced by
 example_write_pdb.py and example_write_h5.py
 '''
-import numpy as np
 from qnd.lazy import openb
 from qnd.frontend import QList
 
 fnames = ["foo*.pdb", "foo.h5"]
 
 for fname in fnames:
-    print("\nOpening file(s): {}".format(fname))
+    print(f"\nOpening file(s): {fname}")
 
     with openb(fname, "r") as f:
         for k in list(f):
